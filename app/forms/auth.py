6aac6c246caf5424a24dd7425534037ba956995b
Rushit PC <rushitchandani@gmail.com>
2026-05-12 23:15:22 +0530

    update login form

--- app/forms/auth.py
+++ app/forms/auth.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
+    submit = SubmitField('Sign In')
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
