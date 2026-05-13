4601e2f2f62d6e26960b5ca2bb01bef7a2e3e03e
Rushit PC <rushitchandani@gmail.com>
2026-05-13 22:33:52 +0530

    fix signup structure

--- app/forms/auth.py
+++ app/forms/auth.py
@@ -11,4 +11,5 @@ class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Register')
+    confirm = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
